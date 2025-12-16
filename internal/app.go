e949fc17b6e1f554aa7c315484c427046ffd9d8f
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-12-16 14:51:22 +0700

    [app] add Uber Fx support

---
+++ internal/app.go
@@ -0,0 +1,43 @@
+package internal
+
+import (
+	"context"
+
+	"github.com/go-core-fx/logger"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+func Run() {
+	fx.New(
+		// CORE MODULES
+		logger.Module(),
+		logger.WithFxDefaultLogger(),
+		// sqlfx.Module(),
+		// goosefx.Module(),
+		// bunfx.Module(),
+		// fiberfx.Module(),
+		//
+		// APP MODULES
+		// config.Module(),
+		// db.Module(),
+		// server.Module(),
+		// bot.Module(),
+		//
+		// BUSINESS MODULES
+		// users.Module(),
+		//
+		fx.Invoke(func(lc fx.Lifecycle, logger *zap.Logger) {
+			lc.Append(fx.Hook{
+				OnStart: func(_ context.Context) error {
+					logger.Info("app started")
+					return nil
+				},
+				OnStop: func(_ context.Context) error {
+					logger.Info("app stopped")
+					return nil
+				},
+			})
+		}),
+	).Run()
+}
